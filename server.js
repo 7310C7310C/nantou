fed9c0293456be48c31e8c0afdb50a1a767ff0a1
7310C7310C <676400126@qq.com>
2025-10-14 04:36:53 +0000

    Revert "增加Helmet配置：允许HTML内联事件处理器（onclick等）"
    This reverts commit 748c0ca82dc8227b64965e57a2bf0594af165150.

--- server.js
+++ server.js
@@ -31,7 +31,6 @@ app.use(helmet({
       defaultSrc: ["'self'"],
       styleSrc: ["'self'", "'unsafe-inline'"],
       scriptSrc: ["'self'", "'unsafe-inline'"],
-      scriptSrcAttr: ["'unsafe-inline'"], // 允许HTML内联事件处理器（onclick等）
       imgSrc: ["'self'", "data:", "https:"],
       connectSrc: ["'self'"],
       fontSrc: ["'self'"],
